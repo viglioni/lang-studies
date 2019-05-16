7a0c081a3e3566bbb545318a119f798327f6452d
Laura <laura.viglioni@clickideia.com.br>
2019-05-16 11:29:00 -0300

    Pegando alterações parciais de bugs

---
+++ number-theory/prime-numbers/Fermat.hs
@@ -0,0 +1,52 @@
+-- @author Laura Viglioni
+-- 2019
+-- GNU General Public License v3.0 
+
+-- :gb:
+-- Fermat's algorithm to test primality
+-- This is a probabilistic algorithm
+-- False means it is NOT a prime
+-- True means it probably is prime
+-- First parameter is a integer to be tested
+-- The second is how many times it must be tested
+
+-- 🇧🇷
+-- Algoritmo de Fermat para testar primalidade
+-- É um algoritmo probabilístico
+-- False significa que o número NÃO é primo
+-- True significa que o número provavelmente é primo
+-- O primeiro parâmetro é um inteiro a ser testado
+-- O segundo é quantas vezes ele deve ser testado
+
+-- @param prime (integer)
+-- @param repeat (integer)
+-- @return d,s,t (integer, integer, integer)
+
+module Fermat where
+
+import System.Random
+import ModularExp
+
+fermat_test :: (Integral t, Random t) => t -> t -> IO Bool
+fermat_test prime repeat =
+  do
+    seed <- newStdGen
+    return $ fermat_aux prime repeat seed 1 True
+
+
+fermat_aux :: (Integral t, RandomGen g, Random t) => t -> t -> g -> t -> Bool -> Bool
+fermat_aux 2 _ _ _ _ = True
+fermat_aux prime repeat seed counter acc =
+  if prime < 2 then False
+  else
+    if counter <= repeat && acc 
+    then fermat_aux prime repeat new_seed (counter+1) test
+    else acc
+  where
+    (a,new_seed) = randomR (2, (prime-1)) seed
+    test = acc && test_prime prime a
+
+test_prime :: (Integral a, Random a) => a -> a -> Bool
+test_prime prime a
+  | mod_exp a (prime-1) prime == 1 = True
+  | otherwise = False
---
+++ number-theory/prime-numbers/MillerRabin.hs
@@ -0,0 +1,56 @@
+-- @author Laura Viglioni
+-- 2019
+-- GNU General Public License v3.0 
+
+-- :gb:
+
+-- 🇧🇷
+
+-- prime_generator
+-- @param a (integer)
+-- @param b (integer)
+-- @return p (integer)
+
+-- prime_gen_by_bits
+-- @param a (integer)
+-- @return p (integer)
+
+module MillerRabin where
+import System.Random
+import ModularExp
+
+miller_rabin_test :: (Integral t, Random t) => t -> t -> IO Bool
+miller_rabin_test prime repeat =
+  do
+    seed <- newStdGen
+    return $ mr_test prime repeat seed 1 True
+
+mr_test :: (Integral t, RandomGen g, Random t) => t -> t -> g -> t -> Bool -> Bool
+mr_test 2 _ _ _ _ = True
+mr_test prime repeat seed counter acc =
+  if prime < 2 then False
+  else
+    if counter <= repeat && acc
+    then mr_test prime repeat new_seed (counter+1) test
+    else acc
+  where
+    (base, new_seed) = randomR (2, (prime-1)) seed
+    test = acc && unitary_test prime base
+
+unitary_test prime base = if (mod prime 2) == 0 then False else result
+  where
+    (a,r) = decompose prime
+    first_exp = mod_exp base a prime
+    exps = foldl (\acc r -> (mod_exp (head acc) 2 prime):acc ) [first_exp] [1..(r-1)]
+    minus_one = mod (-1) prime
+    congruent_to_one = first_exp == 1 || first_exp == -1
+    any_congruent_to_minus_one = foldl (\acc x -> acc || (mod x prime)==minus_one) False exps
+    result = congruent_to_one || any_congruent_to_minus_one
+
+decompose :: (Integral a) => a -> (a,a)
+decompose a = decompose_aux (a-1) 0
+
+decompose_aux :: (Integral a) => a -> a -> (a,a)
+decompose_aux a r
+  | mod a 2 == 1 = (a,r)
+  | otherwise = decompose_aux (div a 2) (r+1)
