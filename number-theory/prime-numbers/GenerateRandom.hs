bf0b1396fb0d85b1fc7ad3a8b0551bff4da09aa9
Laura <laura.viglioni@clickideia.com.br>
2019-05-17 10:41:25 -0300

    description in GenerateRandom

--- number-theory/prime-numbers/GenerateRandom.hs
+++ number-theory/prime-numbers/GenerateRandom.hs
@@ -3,9 +3,17 @@
 -- GNU General Public License v3.0 
 
 -- :gb:
+-- Generates an list with n random integers in the interval [min,max]
 
 -- 🇧🇷
+-- Gera uma lista com n inteiros aleatórios no intervalo [min,max]
 
+-- gen_random_arr
+-- @param min (integer)
+-- @param max (integer)
+-- @param seed (seed)
+-- @param n (integer)
+-- @return [a1,a2,...,an] (arr of integers)
 
 module GenerateRandom where
 
