511a104754a5636558f605857e368c35fdc93b3a
Laura <laura.viglioni@loft.com.br>
2021-06-20 19:08:42 -0300

    delete learning lol

--- learning-lol/app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Lib
-
-main :: IO ()
-main = do
-  someFunc
-  printL
--- learning-lol/src/Lib.hs
+++
@@ -1,8 +0,0 @@
-module Lib
-  ( someFunc,
-  )
-where
-
-someFunc :: IO ()
-someFunc = do
-  putStrLn "hello workd"
